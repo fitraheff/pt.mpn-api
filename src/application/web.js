a551c2774a58e66092abd3f3aaab6f2ae00a483d
fitraheff <mhmdfitrah24@gmail.com>
2025-12-01 14:21:22 +0700

    fix routes

--- src/application/web.js
+++ src/application/web.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { errorMiddleware } from "../middleware/error-middleware.js";
-import { userRouter } from "../routes/api.js";
+import { userRouter } from "../routes/api-users.js";
+import { BURouter } from "../routes/api-BUsaha.js";
 
 export const web = express();
 web.use(express.json());
 
-web.use(userRouter);
+web.use("/api/users", userRouter);
+web.use("/api/bidang-usaha", BURouter);
 
 web.use(errorMiddleware);
 
-web.use("/uploads", express.static("uploads"));
+// web.use("/uploads", express.static("uploads"));
